f82fcba2c16306d31d6ad9e88276c9a8331029d1
Jebitok <sharonkosgei4@gmail.com>
2021-05-08 20:24:15 +0300

    Add CChanges to Domf file

--- src/dom.js
+++ src/dom.js
@@ -50,7 +50,7 @@ const tasksDOM = (index) => {
             while (todowrap.firstChild) {
                 todowrap.removeChild(todowrap.lastChild);
             }
-            todowrap.appendChild(configTodoDOM(index, j, Task));
+            todowrap.appendChild(configTaskDOM(index, j, Task));
         });
         btnDelete.addEventListener('click', () => {
             const todos = document.querySelector('.todos');
@@ -132,5 +132,82 @@ const configTaskDom = (index, j, Task) => {
     radios.classList.add('priority-radios');
     const LowPriority = document.createElement('div');
     LowPriority.classList.add('child-radio');
-    
-}
+    const MediumPriority = document.createElement('div');
+    MediumPriority.classList.add('child-radio');
+    const HighPriority = document.createElement('div');
+    HighPriority.classList.add('child-radio');
+    const radioLowPriority = document.createElement('input');
+    radioLowPriority.addEventListener('click', (e) => {
+        if (e.target.checked) {
+            todoConfig.classList.remove('high-p');
+            todoConfig.classList.remove('low-p');
+            todoConfig.classList.remove('medium-p');
+            selection = radioMediumPriority.value;
+        }
+    });
+    const radioHighPriority = document.createElement('input');
+    radioHighPriority.type = 'radio';
+    radioHighPriority.name = 'priority';
+    radioHighPriority.id = 'rdLow';
+    radioHighPriority.value = 'High';
+    const labelHighPriority = document.createElement('label');
+    labelHighPriority.textContent = 'High';
+    labelHighPriority.htmlFor = 'rdLow';
+    HighPriority.appendChild(labelHighPriority);
+    HighPriority.appendChild(radioHighPriority);
+    radioHighPriority.addEventListener('click', (e) => {
+        if (e.target.checked) {
+            todoConfig.classList.remove('low-p');
+            todoConfig.classList.remove('medium-p');
+            todoConfig.classList.add('high-p');
+            selection = radioHighPriority.value;
+        }
+    });
+    radios.appendChild(LowPriority);
+    radios.appendChild(MediumPriority);
+    radios.appendChild(HighPriority);
+    switch (Task.priority.toLowerCase()) {
+        case 'low': todoConfig.classList.add('low-p'); radioLowPriority.checked = true; selection = radioLowPriority.value; break;
+        case 'medium': todoConfig.classList.add('medium-p'); radioMediumPriority.checked = true;
+            selection = radioMediumPriority.value; break;
+        case 'high': todoConfig.classList.add('high-p'); radioHighPriority.checked = true;
+            selection = radioHighPriority.value; break;
+        default: 'low';
+    }
+    btnSubmit.addEventListener('click', () => {
+        if (inputTitle.value !== '' && inputDesc.value !== '' && inputDate.value !== '' && inputLists.value !== '') {
+            Todo.editTaskFromTodo(index, j, inputTitle.value, inputDesc.value, parse(inputDate.value, "yyyy-MM-dd'T'HH:mm", new Date()), inputLists.value, selection);
+            const todos = document.querySelector('.todos');
+            while (todos.lastChild) {
+                todos.removeChild(todos.lastChild);
+            }
+            todos.appendChild(tasksDOM(index));
+            todos.appendChild(barDOM(index));
+        }
+    });
+    const labelTitle = document.createElement('label');
+    labelTitle.textContent = 'Title:';
+    labelTitle.htmlFor = 'inputTitle';
+    const labelDesc = document.createElement('label');
+    labelDesc.textContent = 'Description';
+    labelDesc.htmlFor = 'inputDesc';
+    const labelDate = document.createElement('label');
+    labelDate.textContent = 'Due Date';
+    labelDate.htmlFor = 'inputDate';
+    const labelLists = document.createElement('label');
+    labelLists.textContent = 'Lists';
+    labelLists.htmlFor = 'inputLists';
+    todoConfig.appendChild(labelTitle);
+    todoConfig.appendChild(inputTitle);
+    todoConfig.appendChild(labelDesc);
+    todoConfig.appendChild(inputDesc);
+    todoConfig.appendChild(labelDate);
+    todoConfig.appendChild(inputDate);
+    todoConfig.appendChild(labelLists);
+    todoConfig.appendChild(inputLists);
+    todoConfig.appendChild(radios);
+    todoConfig.appendChild(btnSubmit);
+    return todoConfig;
+};
+
+export { barDOM, tasksDOM, configTaskDom };
