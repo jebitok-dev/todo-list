2f6191e8bb07af1ff5b0735f99b2ecedd2a7e3cd
Jebitok <sharonkosgei4@gmail.com>
2021-05-14 05:38:11 +0300

    Add Changes to Todo Tests

--- test/todo.test.js
+++ test/todo.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import Todo from '../src/todo';
 import Task from '../src/task';
 
@@ -15,5 +16,15 @@ describe('tests to getAllTasks', () => {
     test('should add task for a todo', () => {
         myTodo.getAllTasks('Developer Communities', 'Join more than one', 'Thursday', 'May 13th, 2021, 23:39', null, true);
         const todos = Task('Developer Communities', 'Join more than one', 'Thursday', 'May 13th, 2021, 23:39', null, true);
-    })
-})
+        expect(todos.length).not.toBeNull();
+    });
+});
+
+describe('tests to removeTask', () => {
+    test('should remove task from todo', () => {
+        myTodo.removeTask('Pick a Stack', 'MERN Stack', 'Friday', 'May 14th, 2021, 5:20', null, true);
+        const todos = Task('Pick a Stack', 'MERN Stack', 'Friday', 'May 14th, 2021, 5:20', null, true);
+        expect(todos.length).not.toBeNull();
+    });
+});
+/* eslint-enable */
